d3b0f3d0e14473bb8fc985a9266048794d3e5b4d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-03-26 18:36:37 +0100

    Added algorithm options

--- TraceGui/ViewModels/MainWindowViewModel.cs
+++ TraceGui/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,15 +16,27 @@ namespace TraceGui.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private SKBitmap? _source;
         private string? _fileName;
         private IEnumerable<PathGeometry>? _paths;
         private int _width;
         private int _height;
+        private int _turdSize = 2;
+        private int _turnPolicy = 4;
+        private double _alphaMax = 1.0;
+        private bool _optiCurve = true;
+        private double _optTolerance = 0.2;
 
         public MainWindowViewModel()
         {
             OpenCommand = ReactiveCommand.CreateFromTask(async () => await OnOpen());
             SaveCommand = ReactiveCommand.CreateFromTask(async () => await OnSave());
+
+            this.WhenAnyValue(x => x.TurdSize).Subscribe(x => Trace());
+            this.WhenAnyValue(x => x.TurnPolicy).Subscribe(x => Trace());
+            this.WhenAnyValue(x => x.AlphaMax).Subscribe(x => Trace());
+            this.WhenAnyValue(x => x.OptiCurve).Subscribe(x => Trace());
+            this.WhenAnyValue(x => x.OptTolerance).Subscribe(x => Trace());
         }
 
         public ICommand OpenCommand { get; }
@@ -52,6 +65,36 @@ namespace TraceGui.ViewModels
         {
             get => _height;
             set => this.RaiseAndSetIfChanged(ref _height, value);
+        }
+
+        public int TurdSize
+        {
+            get => _turdSize;
+            set => this.RaiseAndSetIfChanged(ref _turdSize, value);
+        }
+        
+        public int TurnPolicy
+        {
+            get => _turnPolicy;
+            set => this.RaiseAndSetIfChanged(ref _turnPolicy, value);
+        }
+        
+        public double AlphaMax
+        {
+            get => _alphaMax;
+            set => this.RaiseAndSetIfChanged(ref _alphaMax, value);
+        }
+        
+        public bool OptiCurve
+        {
+            get => _optiCurve;
+            set => this.RaiseAndSetIfChanged(ref _optiCurve, value);
+        }
+
+        public double OptTolerance
+        {
+            get => _optTolerance;
+            set => this.RaiseAndSetIfChanged(ref _optTolerance, value);
         }
 
         private async Task OnOpen()
@@ -106,14 +149,42 @@ namespace TraceGui.ViewModels
 
         private void Load(string filename)
         {
-            using var source = SKBitmap.Decode(filename);
-            var param = new PotraceParam();
-            var paths = PotraceAvalonia.Trace(param, source).ToList();
+            Decode(filename);
 
-            Width = source.Width;
-            Height = source.Height;
-            Paths = paths;
+            Trace();
+
             FileName = filename;
+        }
+
+        private void Decode(string filename)
+        {
+            _source?.Dispose();
+            _source = SKBitmap.Decode(filename);
+        }
+
+        private void Trace()
+        {
+            if (_source is null)
+            {
+                return;
+            }
+
+            var param = new PotraceParam()
+            {
+                TurdSize = _turdSize,
+                TurnPolicy = _turnPolicy,
+                AlphaMax = _alphaMax,
+                OptiCurve = _optiCurve,
+                OptTolerance = _optTolerance,
+            };
+
+            var paths = PotraceAvalonia.Trace(param, _source).ToList();
+
+            Console.WriteLine($"paths {paths.Count}");
+            
+            Width = _source.Width;
+            Height = _source.Height;
+            Paths = paths;
         }
 
         private Window? GetMainWindow()
