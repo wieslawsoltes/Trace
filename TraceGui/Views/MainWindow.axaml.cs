3bc6003b6f663592d1852177f409b8c2f498ef5d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-03-26 19:05:07 +0100

    Update MainWindow.axaml.cs

--- TraceGui/Views/MainWindow.axaml.cs
+++ TraceGui/Views/MainWindow.axaml.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using TraceGui.ViewModels;
 
 namespace TraceGui.Views
 {
@@ -12,6 +16,34 @@ namespace TraceGui.Views
 #if DEBUG
             this.AttachDevTools();
 #endif
+            var dockPanel = this.FindControl<DockPanel>("DockPanel");
+            dockPanel.AddHandler(DragDrop.DragOverEvent, DragOver);
+            dockPanel.AddHandler(DragDrop.DropEvent, Drop);
+        }
+
+        private void DragOver(object? sender, DragEventArgs e)
+        {
+            e.DragEffects = e.DragEffects & (DragDropEffects.Copy | DragDropEffects.Link);
+
+            if (!e.Data.Contains(DataFormats.FileNames))
+            {
+                e.DragEffects = DragDropEffects.None;
+            }
+        }
+
+        private void Drop(object? sender, DragEventArgs e)
+        {
+            if (e.Data.Contains(DataFormats.FileNames))
+            {
+                var fileName = e.Data.GetFileNames()?.FirstOrDefault();
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    if (DataContext is MainWindowViewModel vm)
+                    {
+                        vm.Load(fileName);
+                    }
+                }
+            }
         }
 
         private void InitializeComponent()
