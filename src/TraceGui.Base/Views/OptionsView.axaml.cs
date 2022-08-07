580952e50f770833ee8f6cb8778cd4ec800f7d32
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-08-07 11:24:31 +0200

    Refactor into views

---
+++ src/TraceGui.Base/Views/OptionsView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TraceGui.Views;
+
+public class OptionsView : UserControl
+{
+    public OptionsView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
---
+++ src/TraceGui.Base/Views/PreviewView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TraceGui.Views;
+
+public class PreviewView : UserControl
+{
+    public PreviewView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
+
